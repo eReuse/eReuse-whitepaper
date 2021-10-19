48d7c1a72ea742c8620fcac2e626c8419ab4cf43
David Franquesa <davidfranquesa@gmail.com>
2021-10-19 10:10:23 +0200

    Update index.html

--- main_render.R
+++
@@ -1,5 +0,0 @@
-rmarkdown::render(input = file.path(".","index.Rmd"),
-                  output_file =  file.path(".","index"),
-                  output_format = "pdf_document",
-                  envir = new.env())
-
